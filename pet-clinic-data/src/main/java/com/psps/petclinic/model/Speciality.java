44113a47780fc0c92939672d6f6fb3230d169f61
Santhosh PSPS <santhoshpsp4@gmail.com>
2024-05-05 00:09:25 +0530

    Convert Vets to JPA entities

--- pet-clinic-data/src/main/java/com/psps/petclinic/model/Speciality.java
+++ pet-clinic-data/src/main/java/com/psps/petclinic/model/Speciality.java
@@ -1,6 +1,14 @@
 package com.psps.petclinic.model;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.ManyToMany;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "specialities")
 public class Speciality extends BaseEntity{
+    @Column(name = "description")
     private String description;
 
     public String getDescription() {
